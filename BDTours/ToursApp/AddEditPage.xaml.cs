e59325c133357f5d6599b63690ae783de0d685ad
Ilia <118176700+Mazhorov21Ilia@users.noreply.github.com>
2023-04-06 15:43:24 +0300

    Add files via upload

---
+++ BDTours/ToursApp/AddEditPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _curentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+
+            if (selectedHotel != null)
+            {
+                _curentHotel = selectedHotel;
+            }
+
+            DataContext = _curentHotel;
+            ComboCountries.ItemsSource = tours21ISMazhorovEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_curentHotel.Name))
+                errors.AppendLine("Укажите название отеля:");
+            if (_curentHotel.CountOfStars < 1 || _curentHotel.CountOfStars > 5)
+                errors.AppendLine("Количество звёзд от 1 до 5");
+            if (_curentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_curentHotel.Id == 0)
+                tours21ISMazhorovEntities.GetContext().Hotel.Add(_curentHotel);
+
+
+            try
+            {
+                tours21ISMazhorovEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manger.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ BDTours/ToursApp/HotelsPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+            //DGridHotels.ItemsSource = tours21ISMazhorovEntities.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manger.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manger.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if (MessageBox.Show($"Точно удаляем?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    tours21ISMazhorovEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    tours21ISMazhorovEntities.GetContext().SaveChanges();
+
+                    DGridHotels.ItemsSource = tours21ISMazhorovEntities.GetContext().Hotel.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                tours21ISMazhorovEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = tours21ISMazhorovEntities.GetContext().Hotel.ToList();
+            }
+        }
+    }
+}
